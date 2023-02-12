6620bab5b2b992468b5241f1cff222b1ec6b55e8
Steven Bister <steve.bister@openstudycollege.com>
2023-02-12 15:14:45 +0000

    only changes remix-app

--- apps/remix-app/app/routes/index.tsx
+++ apps/remix-app/app/routes/index.tsx
@@ -5,6 +5,7 @@ export default function Index() {
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix Turborepo Vercel example</h1>
       <p>Makin' changes</p>
+      <p>Only changed this app, this time</p>
       <Button />
     </div>
   );
