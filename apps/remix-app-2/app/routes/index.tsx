409d2b7aed326b4ddb90808fa182d201e117af7a
Steven Bister <steve.bister@openstudycollege.com>
2023-02-12 15:28:32 +0000

    edit remix-app-2

--- apps/remix-app-2/app/routes/index.tsx
+++ apps/remix-app-2/app/routes/index.tsx
@@ -4,6 +4,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix Turborepo Vercel example 2</h1>
+      <p>Changez</p>
       <Button />
     </div>
   );
