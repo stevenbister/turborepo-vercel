6b67fc98e77c34d3ad85957a55541b1696b76480
Steven Bister <steve.bister@openstudycollege.com>
2023-02-12 16:01:05 +0000

    removes some text

--- apps/remix-app/app/routes/index.tsx
+++ apps/remix-app/app/routes/index.tsx
@@ -5,7 +5,6 @@ export default function Index() {
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix Turborepo Vercel example</h1>
       <p>Makin' changes</p>
-      <p>Only changed this app, this time</p>
       <Button />
     </div>
   );
